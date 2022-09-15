978d422ebb95f63f7ce5a487d5a153dda80180af
Calm Lee <calm_lee@naver.com>
2022-09-16 00:38:04 +0900

    기초공사 - 이모티콘 스티커 추가

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
-import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-import RouterTest from './components/RouterTest'
+import RouterTest from "./components/RouterTest";
 
-import Home from './pages/Home'
-import New from './pages/New'
-import Edit from './pages/Edit'
-import Diary from './pages/Diary'
-
+import Home from "./pages/Home";
+import New from "./pages/New";
+import Edit from "./pages/Edit";
+import Diary from "./pages/Diary";
 
 function App() {
+  const env = process.env;
+  env.PUBLIC_URL = env.PUBLIC_URL || "";
+
   return (
     <BrowserRouter>
       <div className="App">
         <h2>App.js</h2>
+        <img src={process.env.PUBLIC_URL + `/assets/emotion1.png`} />
+        <img src={process.env.PUBLIC_URL + `/assets/emotion2.png`} />
+        <img src={process.env.PUBLIC_URL + `/assets/emotion3.png`} />
+        <img src={process.env.PUBLIC_URL + `/assets/emotion4.png`} />
+        <img src={process.env.PUBLIC_URL + `/assets/emotion5.png`} />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/new" element={<New />} />
