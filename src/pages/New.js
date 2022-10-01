21548ca64400b848bd656ab7e2e49891bc219200
Calm Lee <calm_lee@naver.com>
2022-10-01 17:37:16 +0900

    DiaryEditor 화면 구현

--- src/pages/New.js
+++ src/pages/New.js
@@ -1,10 +1,10 @@
-const New = () => {
-    return (
-        <div>
-            <h1>New</h1>
-            <p>이 곳은 일기 작성 페이지입니다.</p>
-        </div>
-    )
-}
+import DiaryEditor from "./../components/DiaryEditor";
 
+const New = () => {
+  return (
+    <div>
+      <DiaryEditor />
+    </div>
+  );
+};
 export default New;
