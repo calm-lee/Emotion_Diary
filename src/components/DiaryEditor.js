2f4e50c1dd4ebe754552d13357c86e49513425ed
Calm Lee <calm_lee@naver.com>
2022-10-04 08:00:25 +0900

    DiaryEditor 구현

--- src/components/DiaryEditor.js
+++ src/components/DiaryEditor.js
@@ -2,6 +2,38 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import MyButton from "./MyButton";
 import MyHeader from "./MyHeader";
+import EmotionItem from "./EmotionItem";
+
+const env = process.env;
+env.PUBLIC_URL = env.PUBLIC_URL || "";
+
+const emotionList = [
+  {
+    emotion_id: 1,
+    emotion_img: process.env.PUBLIC_URL + "/assets/emotion1.png",
+    emotion_descript: "완전 좋음",
+  },
+  {
+    emotion_id: 2,
+    emotion_img: process.env.PUBLIC_URL + "/assets/emotion2.png",
+    emotion_descript: "좋음",
+  },
+  {
+    emotion_id: 3,
+    emotion_img: process.env.PUBLIC_URL + "/assets/emotion3.png",
+    emotion_descript: "그럭저럭",
+  },
+  {
+    emotion_id: 4,
+    emotion_img: process.env.PUBLIC_URL + "/assets/emotion4.png",
+    emotion_descript: "나쁨",
+  },
+  {
+    emotion_id: 5,
+    emotion_img: process.env.PUBLIC_URL + "/assets/emotion5.png",
+    emotion_descript: "끔찍함",
+  },
+];
 
 export const getStringDate = (date) => {
   let year = date.getFullYear();
@@ -29,16 +61,24 @@ const DiaryEditor = () => {
           <MyButton text={"< 뒤로 가기"} onClick={() => navigate(-1)} />
         }
       />
-      <div>
+      <div className="DiaryEditor">
         <section>
           <h4>오늘은 언제인가요?</h4>
-          <div className="input-box">
+          <div className="input_box">
             <input
-              className="input-date"
+              className="input_date"
               value={date}
               onChange={(e) => setDate(e.target.value)}
               type="date"
             />
+          </div>
+        </section>
+        <section>
+          <h4>오늘의 감정</h4>
+          <div className="input_box emotion_list_wrapper">
+            {emotionList.map((it) => (
+              <EmotionItem key={it.emotion_id} />
+            ))}
           </div>
         </section>
       </div>
