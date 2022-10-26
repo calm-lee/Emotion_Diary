aad79cbcc91282619f3194c2b9355ec15ce56984
Calm Lee <calm_lee@naver.com>
2022-10-27 07:58:16 +0900

    Diary 상세 페이지 emotion 관련 기능 구현

--- src/pages/Diary.js
+++ src/pages/Diary.js
@@ -3,6 +3,8 @@ import { useContext } from "react";
 import { useState, useEffect } from "react";
 import { DiaryStateContext } from "../App";
 import { getStringDate } from "../util/date.js";
+import { emotionList } from "../util/emotion.js";
+
 import MyHeader from "../components/MyHeader";
 import MyButton from "../components/MyButton";
 
@@ -30,6 +32,11 @@ const Diary = () => {
   if (!data) {
     return <div className="DiaryPage">로딩 중입니다...</div>;
   } else {
+    const curEmotionData = emotionList.find(
+      (it) => parseInt(it.emotion_id) === parseInt(data.emotion)
+    );
+    console.log(curEmotionData);
+
     return (
       <div className="DiaryPage">
         <MyHeader
@@ -37,7 +44,21 @@ const Diary = () => {
           leftChild={
             <MyButton text="< 뒤로가기" onClick={() => navigate(-1)} />
           }
+          rightChild={
+            <MyButton
+              text="수정하기"
+              onClick={() => navigate(`/edit/${data.id}`)}
+            />
+          }
         />
+        <article>
+          <section>
+            <h4>오늘의 감정</h4>
+            <div className="diary_img_wrapper">
+              <img src={curEmotionData.emotion_img} />
+            </div>
+          </section>
+        </article>
       </div>
     );
   }
